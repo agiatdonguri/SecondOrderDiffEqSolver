3f593d2a879111357f9ecb0d4782dd975ae0c233
Taiga Nishida <tainish@nuevaschool.org>
2022-12-08 12:18:37 -0800

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,13 +1,25 @@
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 import numpy as np
+import math
+
+q = 1.5 * math.pow(10, -9)
+V = 7984.03193613
+d = 0.176
+b = 1
+L = 0.165
+m = 0.00038
+g = 9.8
+
+
+Fe = q * V / d# + k * q * q / math.pow((d /2 - x), 2)
 
 def f(u, x):
-    return (u[1], -5 * u[1] - 7)
+    return (u[1], -b * u[1] / m + Fe * math.cos(u[0]) / m / L - g * math.sin(u[0]) / L)
     
 y0 = [0, 0] # initial conditions
 
-xs = np.arange(0, 10, 0.0001)
+xs = np.arange(0, 15, 0.001)
 us = odeint(f, y0, xs)
 ys = us[:, 0]
 
@@ -17,3 +29,4 @@ plt.xlabel('t')
 plt.ylabel('theta')
 plt.title('gg ez')
 plt.show()
+
